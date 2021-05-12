f6e07bc7f128f03a5263553996b0d196f1846167
Max Parastiuk <parastukm@gmail.com>
2021-05-12 13:10:55 +0300

    MASTER-3 : template seventh

---
+++ src/main/java/homework11/task1/MyStAXParser.java
@@ -0,0 +1,46 @@
+package homework11.task1;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.*;
+import javax.xml.transform.Result;
+import java.io.*;
+
+public class MyStAXParser {
+    public static void main(String[] args) {
+
+        try {
+            File f = new File("C:\\Users\\User\\IdeaProjects\\java_professional\\src\\main\\java\\homework11\\task1\\City.xml");
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+            XMLStreamWriter writer = outputFactory.createXMLStreamWriter(new FileOutputStream(f));
+
+            writer.writeStartDocument("UTF-8","1.0");
+            writer.writeStartElement("location");
+            writer.writeStartElement("city");
+            writer.writeAttribute("size","big");
+            writer.writeCharacters("Kiev");
+            writer.writeEndElement();
+            writer.writeStartElement("street");
+            writer.writeCharacters("Khreshchatyk");
+            writer.writeEndElement();
+            writer.writeStartElement("house_number");
+            writer.writeCharacters("10");
+            writer.writeEndElement();
+
+
+            writer.writeEndDocument();
+
+            writer.flush();
+            writer.close();
+
+        } catch (XMLStreamException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/homework11/task2/AnimalParser.java
@@ -0,0 +1,51 @@
+package homework11.task2;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
+public class AnimalParser {
+    public static void main(String[] args) {
+        final String fileName = "C:\\Users\\User\\IdeaProjects\\java_professional\\src\\main\\java\\homework11\\task2\\Animal.xml";
+
+
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+
+            DefaultHandler handler = new DefaultHandler() {
+
+                boolean name = false;
+                boolean breed = false;
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    if (qName.equalsIgnoreCase("name")) {
+                        name = true;
+                    } if (qName.equalsIgnoreCase("breed")) {
+                      breed = true;
+                    }
+                }
+
+                @Override
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    if(name) {
+                        System.out.println("Name: " + new String(ch,start,length));
+                        name = false;
+                    } if(breed){
+                        System.out.println("Breed: " + new String(ch,start,length));
+                        breed = false;
+                    }
+                }
+            };
+            saxParser.parse(fileName,handler);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
